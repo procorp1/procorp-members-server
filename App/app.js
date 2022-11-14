718b38333c89058d3c03ea61bf220cf2958552ed
shariarhossain23e <shariarhossain23@gamil.comm>
2022-11-14 12:16:46 +0600

    initial setup

--- App/app.js
+++ App/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config()
 const app = express()
+
 
 
 
@@ -9,6 +11,14 @@ app.use(cors())
 app.use(express.json())
 
 
+
+
+app.get('/',(req,res)=> {
+    res.send("procorp members server running")
+})
+app.get('/health',(req,res)=> {
+    res.send("health ok")
+})
 
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const port = process.env.PORT
-const colors = require("colors")
+const port = process.env.PORT || 8800;
+const colors = require("colors");
 
-const app = require('./App/app')
+const app = require("./App/app");
 
-
-
-
-app.listen(port,()=> {
-    console.log(colors.red(`procorp members server running`).bold);
-})
+app.listen(port, () => {
+  console.log(colors.red(`procorp members server running`).bold);
+});
